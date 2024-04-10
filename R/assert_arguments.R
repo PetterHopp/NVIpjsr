7de82b997ca1ed89e97a87593e63441d3e2e79c3
Petter Hopp <Petter.Hopp@vetinst.no>
2024-04-10 07:41:54 +0200

    refactor: included assert_read_functions from NVIdb

--- R/assert_arguments.R
+++ R/assert_arguments.R
@@ -1,6 +1,7 @@
 # assert_arguments
 #
 # assert collections that developed to fit functions with standardized input arguments.
+# - assert_read_function
 # - assert_copy_function
 #
 #' @title Collection of assertions for copy_functions
@@ -61,4 +62,164 @@ assert_copy_function <- function(filename,
 
   # Report check-results
   checkmate::reportAssertions(checks)
+}
+
+# #' @title Collection of assertions for add_functions
+# #' @description Collection of assertions used in standard add_functions.
+# #'
+# #' @details All add functions except one, have the same arguments and the
+# #'     assertion can be standardized.
+# #'
+# #' The assertCollection must be set up separately and is used as input argument, see example.
+# #'
+# #' @param data Argument to the add-function to be asserted.
+# #' @param translation_table Argument to the add-function to be asserted.
+# #' @param code_column Argument to the add-function to be asserted.
+# #' @param new_column Argument to the add-function to be asserted.
+# #' @param overwrite Argument to the add-function to be asserted.
+# #' @param add assertCollection.
+# #'
+# #' @return An assertCollection that have been updated with the results
+# #'     of assertions for \code{data}, \code{translation_table},
+# #'     \code{code_column}, \code{new_column} and \code{overwrite}.
+# #'
+# #' @author Petter Hopp Petter.Hopp@@vetinst.no
+# #' @keywords internal
+# #' @export
+# #'
+# assert_add_functions <- function(data,
+#                                  translation_table,
+#                                  code_column,
+#                                  new_column,
+#                                  overwrite,
+#                                  add) {
+#
+#   # ARGUMENT CHECKING ----
+#   # Perform checks
+#   # data
+#   checkmate::assert_data_frame(data, add = add)
+#   # translation_table
+#   checkmate::assert_data_frame(translation_table, add = add)
+#   # code_column
+#   checkmate::assert_vector(code_column, any.missing = FALSE, len = 1, add = add)
+#   NVIcheckmate::assert_names(unname(code_column),
+#                              type = "named",
+#                              subset.of = colnames(translation_table),
+#                              comment = paste0("The value for code_column must be a column in the translation table, ",
+#                                               #  deparse(substitute(translation_table)),
+#                                               "but '",
+#                                               unname(code_column),
+#                                               "' is not a column name in the translation table"),
+#                              add = add)
+#   NVIcheckmate::assert_names(names(code_column),
+#                              type = "named",
+#                              subset.of = colnames(data),
+#                              comment = paste0("The name of the code_column must be a column in the data",
+#                                               # deparse(substitute(data)),
+#                                               ", but '",
+#                                               base::setdiff(names(code_column), colnames(data)),
+#                                               "' is not a column in the data. You must name the code_column ",
+#                                               "if the code_column in data is different from in the translation table"),
+#                              add = add)
+#   # new_column
+#   checkmate::assert_vector(new_column, any.missing = FALSE, min.len = 1, add = add)
+#   NVIcheckmate::assert_names(unname(new_column),
+#                              type = "named",
+#                              subset.of = colnames(translation_table),
+#                              comment = paste0("The value(s) for new_column must be column name(s) in the translation table",
+#                                               # deparse(substitute(translation_table)),
+#                                               ", but '",
+#                                               unname(new_column),
+#                                               "' are not column name(s) in the translation table"),
+#                              add = add)
+#   if (isFALSE(overwrite)) {
+#     NVIcheckmate::assert_names(names(new_column),
+#                                type = "named",
+#                                disjunct.from = setdiff(colnames(data), code_column),
+#                                comment = paste0("The column name(s): '",
+#                                                 intersect(colnames(data), names(new_column)),
+#                                                 "' already exist in '",
+#                                                 deparse(substitute(data)),
+#                                                 "`. Either give new column name(s) for the column(s) called '",
+#                                                 intersect(colnames(data), names(new_column)),
+#                                                 "' or specify overwrite = TRUE to replace values in the existing column(s) with new content"),
+#                                add = add)
+#   }
+#   NVIcheckmate::assert_names(names(new_column),
+#                              type = "named",
+#                              disjunct.from = names(code_column),
+#                              comment = paste0("You cannot give any of the new column(s) the same name as the code_column '",
+#                                               names(code_column),
+#                                               "' in the data" # ,
+#                                               # deparse(substitute(data)), "`"
+#                              ),
+#                              add = add)
+#
+#   # overwrite
+#   checkmate::assert_logical(overwrite, any.missing = FALSE, len = 1, add = add)
+#
+#   return(add)
+# }
+
+
+#' @title Collection of assertions for read_functions
+#' @description Collection of assertions used in standard read_functions.
+#'
+#' @details All read functions include the same arguments and the
+#'     assertion can be standardized. The assertion of \code{filname} is
+#'     constructed to handle both character and list input.
+#'
+#'     The assertion is based on removing ending "\\" and "/" from
+#'     \code{from_path} before the assertion is performed.
+
+#' The assertCollection must be set up separately and is used as input argument, see example.
+#'
+#' @param filename Argument to the function to be asserted.
+#' @param from_path Argument to the function to be asserted. Ending
+#'     "\\" and "/" must have been removed before the assertion is performed.
+#' @param add assertCollection.
+#'
+#' @return An assertCollection that have been updated with the results of assertions for \code{filename} and \code{from_path}.
+#'
+#' @author Petter Hopp Petter.Hopp@@vetinst.no
+#' @keywords internal
+#' @export
+#'
+#' @examples
+#' # Attach package and make temporary directory
+#' library(NVIdb)
+#' td <- tempdir()
+#' write.csv2(NVIdb::PJS_levels, file = file.path(td, "PJS_levels.csv"))
+#'
+#' # ARGUMENT CHECKING
+#' # Object to store check-results
+#' checks <- checkmate::makeAssertCollection()
+#' # Perform checks
+#' checks <- assert_read_functions(filename = "PJS_levels.csv",
+#'                                 from_path = td,
+#'                                 add = checks)
+#' # Report check-results
+#' checkmate::reportAssertions(checks)
+assert_read_functions <- function(filename,
+                                  from_path,
+                                  add) {
+
+  checkmate::assert_class(x = add, classes = "AssertCollection")
+
+  # Perform checks ----
+
+  ## filename
+  checkmate::assert(checkmate::check_character(filename,
+                                               min.chars = 1, len = 1,
+                                               any.missing = FALSE),
+                    checkmate::check_list(filename,
+                                          min.len = 1,
+                                          any.missing = FALSE),
+                    combine = "or",
+                    add = add)
+  ## from_path / filename
+  for (i in c(1:length(filename))) {
+    checkmate::assert_file_exists(file.path(from_path, filename[[i]]), access = "r", add = add)
+  }
+  return(add)
 }
