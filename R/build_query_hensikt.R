425b422bc1b3971e5d22113896896b0944529c30
Petter Hopp <Petter.Hopp@vetinst.no>
2024-04-29 08:22:59 +0200

    style: styled help for build_query_hensikt

--- R/build_query_hensikt.R
+++ R/build_query_hensikt.R
@@ -12,9 +12,9 @@
 #'     should be included, end the code with \%.
 #' @template build_query_db
 #'
-#' @return A list with select-statements for "v2_sak_m_res" and
-#'     "v_sakskonklusjon", respectively. The statements should be
-#'     included in a \code{RODBC::sqlQuery}.
+#' @return A list with select-statements for "v2_sak_m_res" and "v_sakskonklusjon",
+#'     respectively. The statements can thereafter be included in a
+#'     \ifelse{html}{\code{\link[RODBC:sqlQuery]{RODBC::sqlQuery}}}{\code{RODBC::sqlQuery}}.
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #'
