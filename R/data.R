e3901a554163aab752943358ff5a5e3bec453333
Petter Hopp <Petter.Hopp@vetinst.no>
2024-04-29 08:25:32 +0200

    style: styled help for PJS_levels and PJS_code_description_colname

--- R/data.R
+++ R/data.R
@@ -2,8 +2,8 @@
 #'
 #' @description A data frame with the variable names (column names) in PJS and their corresponding PJS-level. The column names
 #'    are the standardized column names, i.e. after running \code{NVIdb::standardize_columns}.
-#'    The raw data can be edited in the \code{./data-raw/PJS_levels.xlsx} and the the code for preparing of the data frame is written in
-#'    \code{./data-raw/generate_PJS_levels.R}. The \code{PJS_levels} is used as input for \code{NVIdb::select_PJS_levels}.
+#'    The raw data can be edited in the "./data-raw/PJS_levels.xlsx" and the the code for preparing of the data frame is written in
+#'   "./data-raw/generate_PJS_levels.R". The \code{PJS_levels} is used as input for \code{NVIdb::select_PJS_levels}.
 #'
 #' @details The variables included into a specific level is given the value 1, if not included they are given the value 0. To
 #'    ensure that information on a specific level an be traced to the correct sak, all index variables are given value 1.
@@ -20,7 +20,7 @@
 #'   \item{subundersokelse}{columns at subundersokelse-level are given value 1}
 #'   \item{subresultat}{columns at subresultat-level are given value 1}
 #' }
-#' @source \code{./data-raw/PJS_levels.xlsx} in package \code{NVIdb}
+#' @source "./data-raw/PJS_levels.xlsx" in package \code{NVIdb}
 "PJS_levels"
 
 
@@ -34,7 +34,7 @@
 #'    column names of the code variable are the standardised column names, i.e.
 #'    after running \code{NVIdb::standardize_columns}.
 #'
-#'    The raw data can be edited in the \code{./data-raw/generate_PJS_code_description_colname.R}.
+#'    The raw data can be edited in the "./data-raw/generate_PJS_code_description_colname.R".
 #'    The \code{PJS_code_description_colname} is used by \code{NVIdb::add_PJS_code_description}
 #'    when using the options \code{PJS_variable_type = "auto"} and/or
 #'    \code{new_column = "auto"}.
@@ -45,5 +45,5 @@
 #'   \item{type}{the type of PJS variable as used by \code{NVIdb::add_PJS_code_description} to translate PJS-codes to description text}
 #'   \item{new_column}{The new standard column names for the corresponding code column name in PJS}
 #' }
-#' @source \code{./data-raw/generate_PJS_code_description_colname.R} in package \code{NVIdb}
+#' @source "./data-raw/generate_PJS_code_description_colname.R" in package \code{NVIdb}
 "PJS_code_description_colname"
