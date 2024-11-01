4c0a31bc630432ddafd01ef48d705e0582a2d37a
Petter Hopp <petter.hopp@vetinst.no>
2024-11-01 06:51:20 +0100

    fix: updated correction in set_disease_parameters

--- R/set_disease_parameters.R
+++ R/set_disease_parameters.R
@@ -168,7 +168,7 @@ set_disease_parameters <- function(purpose = NULL,
                              script)]
 
       for (i in 1:length(script)) {
-        sub("[[:blank:]]*[<\\-][[:blank:]]*", " <- ", script[i])
+        sub("[[:blank:]]*<\\-[[:blank:]]*", " <- ", script[i])
         sub("[[:blank:]]*[=][[:blank:]]*", " = ", script[i])
         eval(parse(text = script[i]))
       }
