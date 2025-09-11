ad64127a9e1ccc88c826d7cbc77335b0e2c26fd7
Petter Hopp <Petter.Hopp@vetinst.no>
2025-09-11 09:41:07 +0200

    test: commented out 2 tests in retrieve_PJSdata as takes too much time

--- tests/testthat/test_retrieve_PJSdata.R
+++ tests/testthat/test_retrieve_PJSdata.R
@@ -8,31 +8,29 @@ test_that("retrieve_PJSdata", {
 
   # Read PJSdata using build_query_hensikt
   selection_parameters <- set_disease_parameters(hensikt2select = c("02001"),
-                                                 FUN =  build_query_hensikt)
+                                                 FUN = build_query_hensikt)
   PJSdata <- retrieve_PJSdata(year = 2015,
                               selection_parameters = selection_parameters)
   expect_data_frame(PJSdata[[1]], nrows = 0)
   expect_data_frame(PJSdata[[2]], nrows = 0)
 
-  # Read PJSdata using build_query_one_disease
-  selection_parameters <- set_disease_parameters(analytt2select = c("100201060101"),
-                                                 FUN =  build_query_one_disease)
-  PJSdata <- retrieve_PJSdata(year = 2020,
-                              selection_parameters = selection_parameters)
-  expect_data_frame(PJSdata[[1]], nrows = 17)
-  expect_data_frame(PJSdata[[2]], nrows = 0)
+  # # Read PJSdata using build_query_one_disease
+  # selection_parameters <- set_disease_parameters(analytt2select = c("100201060101"),
+  #                                                FUN = build_query_one_disease)
+  # PJSdata <- retrieve_PJSdata(year = 2020,
+  #                             selection_parameters = selection_parameters)
+  # expect_data_frame(PJSdata[[1]], nrows = 17)
+  # expect_data_frame(PJSdata[[2]], nrows = 0)
 
-  # Read PJSdata using build_query_outbreak
-  selection_parameters <- set_disease_parameters(utbrudd2select = c("10"),
-                                                 hensikt2select = "0100104010",
-                                                 analytt2select = "041503020401",
-                                                 FUN =  build_query_outbreak)
-  PJSdata <- retrieve_PJSdata(year = 2023,
-                              selection_parameters = selection_parameters)
-  expect_data_frame(PJSdata[[1]], nrows = 205)
-  expect_data_frame(PJSdata[[2]], nrows = 0)
-)
-
+  # # Read PJSdata using build_query_outbreak
+  # selection_parameters <- set_disease_parameters(utbrudd2select = c("10"),
+  #                                                hensikt2select = "0100104010",
+  #                                                analytt2select = "041503020401",
+  #                                                FUN =  build_query_outbreak)
+  # PJSdata <- retrieve_PJSdata(year = 2023,
+  #                             selection_parameters = selection_parameters)
+  # expect_data_frame(PJSdata[[1]], nrows = 205)
+  # expect_data_frame(PJSdata[[2]], nrows = 0)
 })
 
 
