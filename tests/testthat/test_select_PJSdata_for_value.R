e479748c90eaf5b1e761da63cae90bd70975dcbd
Petter Hopp <petter.hopp@vetinst.no>
2026-04-24 12:48:01 +0200

    test: corrected test_selected_PJSdata_for_value

--- tests/testthat/test_select_PJSdata_for_value.R
+++ tests/testthat/test_select_PJSdata_for_value.R
@@ -1,58 +1,58 @@
-# library(NVIdb)
+# library(NVIpjsr)
 library(testthat)
 
 test_that("test select_PJSdata_for_value", {
 
-  df <- as.data.frame(cbind(saksnr = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-    code1 = c("01", "01", "01001", "01001", "02", "02", "02001", "02002", "0200201", NA),
-                      code2 = c("03", "03", "03003", "03004", "04", "05", "04003", "04004", "0400403", NA)))
+  df <- data.frame("saksnr" = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                   "code1" = c("01", "01", "01001", "01001", "0102", "02", "02001", "02002", "0200201", NA),
+                   "code2" = c("03", "03", "03003", "03004", "04", "05", "04003", "04004", "0400403", NA))
 
-  correct_result <- as.data.frame(cbind(saksnr = c(1, 2),
-                                        code1 = c("01", "01"),
-                      code2 = c("03", "03")))
-
-  expect_identical(select_PJSdata_for_value(data = df,
-                                      code_column = "code1",
-                                      value_2_check = "01",
-                                      keep_selected = TRUE),
-                   correct_result)
-
-  correct_result <- as.data.frame(cbind(saksnr = c(1, 2, 3, 4),
-                                        code1 = c("01", "01", "01001", "01001"),
-                      code2 = c("03", "03", "03003", "03004")))
+  correct_result <- data.frame("saksnr" = c(1, 2),
+                               "code1" = c("01", "01"),
+                               "code2" = c("03", "03"))
 
   expect_identical(select_PJSdata_for_value(data = df,
-                                      code_column = "code1",
-                                      value_2_check = "01%",
-                                      keep_selected = TRUE),
+                                            code_column = "code1",
+                                            value_2_check = "01",
+                                            keep_selected = TRUE),
                    correct_result)
 
-  correct_result <- as.data.frame(cbind(saksnr = c(10),
-                                        code1 = c(NA_character_),
-                      code2 = c(NA_character_)))
+  correct_result <- data.frame("saksnr" = c(1, 2, 3, 4, 5),
+                               "code1" = c("01", "01", "01001", "01001", "0102"),
+                               "code2" = c("03", "03", "03003", "03004", "04"))
 
   expect_identical(select_PJSdata_for_value(data = df,
-                                      code_column = "code1",
-                                      value_2_check = "NA",
-                                      keep_selected = TRUE),
+                                            code_column = "code1",
+                                            value_2_check = "01%",
+                                            keep_selected = TRUE),
+                   correct_result)
+
+  correct_result <- data.frame("saksnr" = c(10),
+                               "code1" = c(NA_character_),
+                               "code2" = c(NA_character_))
+
+  expect_identical(select_PJSdata_for_value(data = df,
+                                            code_column = "code1",
+                                            value_2_check = "NA",
+                                            keep_selected = TRUE),
                    correct_result)
 
 
-    correct_result <- as.data.frame(cbind(saksnr = c(1, 2, 5, 6, 7, 8, 9, 10),
-                                        code1 = c("01", "01", "02", "02", "02001", "02002", "0200201", NA_character_),
-                                        code2 = c("03", "03", "04", "05", "04003", "04004", "0400403", NA_character_)))
+  correct_result <- data.frame("saksnr" = c(1, 2, 6, 7, 8, 9, 10),
+                               "code1" = c("01", "01", "02", "02001", "02002", "0200201", NA_character_),
+                               "code2" = c("03", "03", "05", "04003", "04004", "0400403", NA_character_))
 
-    result <- select_PJSdata_for_value(data = df,
-                                      code_column = "code1",
-                                      value_2_check = c("01", "02%", "NA"),
-                                      keep_selected = TRUE)
-    rownames(result) <- NULL
+  result <- select_PJSdata_for_value(data = df,
+                                     code_column = "code1",
+                                     value_2_check = c("01", "02%", "NA"),
+                                     keep_selected = TRUE)
+  rownames(result) <- NULL
   expect_identical(result,
                    correct_result)
 
-  correct_result <- as.data.frame(cbind(saksnr = c(3, 4, 5, 6, 7, 8, 9, 10),
-                                        code1 = c("01001", "01001", "02", "02", "02001", "02002", "0200201", NA),
-                                        code2 = c("03003", "03004", "04", "05", "04003", "04004", "0400403", NA)))
+  correct_result <- data.frame("saksnr" = c(3, 4, 5, 6, 7, 8, 9, 10),
+                               "code1" = c("01001", "01001", "0102", "02", "02001", "02002", "0200201", NA),
+                               "code2" = c("03003", "03004", "04", "05", "04003", "04004", "0400403", NA))
 
   result <- select_PJSdata_for_value(data = df,
                                      code_column = "code1",
@@ -62,9 +62,9 @@ test_that("test select_PJSdata_for_value", {
   expect_identical(result,
                    correct_result)
 
-  correct_result <- as.data.frame(cbind(saksnr = c(3, 4),
-                                        code1 = c("01001", "01001"),
-                                        code2 = c("03003", "03004")))
+  correct_result <- data.frame("saksnr" = c(3, 4, 5),
+                               "code1" = c("01001", "01001", "0102"),
+                               "code2" = c("03003", "03004", "04"))
 
   result <- select_PJSdata_for_value(data = df,
                                      code_column = "code1",
@@ -75,25 +75,36 @@ test_that("test select_PJSdata_for_value", {
                    correct_result)
 
 
-    correct_result <- as.data.frame(cbind(saksnr = c(1, 2),
-                                        code1 = c("01", "01"),
-                                        code2 = c("03", "03")))
+  correct_result <- data.frame("saksnr" = c(1, 2),
+                               "code1" = c("01", "01"),
+                               "code2" = c("03", "03"))
 
-    expect_identical(select_PJSdata_for_value(data = df,
-                                              code_column = c("code1", "code2"),
-                                              value_2_check = "01%-03",
-                                              keep_selected = TRUE),
-                     correct_result)
+  expect_identical(select_PJSdata_for_value(data = df,
+                                            code_column = c("code1", "code2"),
+                                            value_2_check = c("01", "03"),
+                                            keep_selected = TRUE),
+                   correct_result)
 
-    correct_result <- as.data.frame(cbind(saksnr = c(1, 2, 3, 4),
-                                          code1 = c("01", "01", "01001", "01001"),
-                                          code2 = c("03", "03", "03003", "03004")))
+  correct_result <- data.frame("saksnr" = c(1, 2, 3, 4, 5),
+                               "code1" = c("01", "01", "01001", "01001", "0102"),
+                               "code2" = c("03", "03", "03003", "03004", "04"))
 
-    expect_identical(select_PJSdata_for_value(data = df,
-                                              code_column = c("code1", "code2"),
-                                              value_2_check = "01%-03%",
-                                              keep_selected = TRUE),
-                     correct_result)
+
+  expect_identical(select_PJSdata_for_value(data = df,
+                                            code_column = c("code1", "code2"),
+                                            value_2_check = c("01%", "03"),
+                                            keep_selected = TRUE),
+                   correct_result)
+
+  correct_result <- data.frame("saksnr" = c(1, 2, 3, 4),
+                                "code1" = c("01", "01", "01001", "01001"),
+                                "code2" = c("03", "03", "03003", "03004"))
+
+  expect_identical(select_PJSdata_for_value(data = df,
+                                            code_column = c("code1", "code2"),
+                                            value_2_check = c("01", "03%"),
+                                            keep_selected = TRUE),
+                   correct_result)
 
 
 })
@@ -103,9 +114,9 @@ test_that("Errors or warnings for select_PJSdata_for_value", {
   linewidth <- options("width")
   options(width = 80)
 
-  df <- as.data.frame(cbind(saksnr = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
-                            code1 = c("01", "01", "01001", "01001", "02", "02", "02001", "02002", "0200201", NA),
-                            code2 = c("03", "03", "03003", "03004", "04", "05", "04003", "04004", "0400403", NA)))
+  df <- data.frame("saksnr" = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                   "code1" = c("01", "01", "01001", "01001", "02", "02", "02001", "02002", "0200201", NA),
+                   "code2" = c("03", "03", "03003", "03004", "04", "05", "04003", "04004", "0400403", NA))
 
   expect_error(select_PJSdata_for_value(data = "df",
                                         code_column = "code1",
